c5db9a89da4d5a648f3f1aec2e39ac3a2ff61014
Pierre.bonnefis <pierre.bonnefis@ormex.io>
2023-10-23 18:04:37 +0200

    Commit vue Menu

--- src/main/java/bts/sio/projet/MenuController.java
+++ src/main/java/bts/sio/projet/MenuController.java
@@ -1,6 +1,22 @@
 package bts.sio.projet;
 
+import javafx.scene.control.Button;
+
 public class MenuController
 {
 
+    @javafx.fxml.FXML
+    private Button btnCreerDemande;
+    @javafx.fxml.FXML
+    private Button btnModifDemande;
+    @javafx.fxml.FXML
+    private Button btnVoirDemande;
+    @javafx.fxml.FXML
+    private Button btnEnregistrerComp;
+    @javafx.fxml.FXML
+    private Button btnModifComp;
+    @javafx.fxml.FXML
+    private Button btnVoirComp;
+    @javafx.fxml.FXML
+    private Button btnVoirStats;
 }
